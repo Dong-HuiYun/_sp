81a7557e790c1c5da063eb998725004220c5ea56
董惠筠 <s111310531@student.nqu.edu.tw>
2026-06-03 01:51:32 +0800

    add homework

--- 虛擬機2/main.py
+++
@@ -1,9 +0,0 @@
-from vm        import VirtualMachine
-from os_kernel import SimpleOS
-from shell     import Shell
-
-
-def main():
-    vm=VirtualMachine(); os_kernel=SimpleOS(vm); Shell(os_kernel).run()
-
-if __name__=="__main__": main()
